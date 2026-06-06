52ee6cea58e9fdd614db2c8e3fce1e98ca641fd1
Manu-Dev-Arya <manudevarya26@gmail.com>
2026-06-06 09:41:32 +0530

    Leetcode 2527

---
+++ Bit/2527. Find Xor-Beauty of Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int xorBeauty(int[] nums) {
+        int n = nums.length;
+        int xorRes = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < n; k++){
+                    int ele1 = nums[i];
+                    int ele2 = nums[j];
+                    int ele3 = nums[k];
+                    int num = (ele1 | ele2) & ele3;
+                    xorRes = xorRes ^ num;
+                }
+            }
+        }
+        return xorRes;
+    }
+}
