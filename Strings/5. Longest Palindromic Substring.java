4b0bd3fe5b029ca9104c5b89a67a6ea82a42ef70
Manu-Dev-Arya <manudevarya26@gmail.com>
2026-06-13 17:32:53 +0530

    Added Optimal Apprach

--- Strings/5. Longest Palindromic Substring.java
+++ Strings/5. Longest Palindromic Substring.java
@@ -40,3 +40,36 @@ class Solution {
     }
 }
 
+
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        int[] ans = new int[] { 0, 0 };
+        boolean[][] dp = new boolean[len][len];
+        for(int i = 0; i < len; i++){
+            dp[i][i] = true;
+        }    
+        for(int i = 1; i < len; i++){
+            if(s.charAt(i-1) == s.charAt(i)){
+                dp[i-1][i] = true;
+                ans[0] = i-1;
+                ans[1] = i;
+            }
+        }
+        
+        for (int diff = 2; diff < len; diff++) {
+            for (int i = 0; i < len - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        int i = ans[0];
+        int j = ans[1];
+        return s.substring(i, j + 1);
+    }
+}
