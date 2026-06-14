ad29241a12782ead61f749d3a8ae9968776cd690
Manu-Dev-Arya <manudevarya26@gmail.com>
2026-06-14 10:18:08 +0530

    Added Optimal solution

--- LinkedList/2130. Maximum Twin Sum of a Linked List.java
+++ LinkedList/2130. Maximum Twin Sum of a Linked List.java
@@ -28,3 +28,34 @@ class Solution {
         return max;
     }
 }
+
+
+class Solution {
+    public int pairSum(ListNode head) {
+        int len = 0;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            len++;
+        }
+        Stack<Integer> st = new Stack<>();
+        int i = 0;
+        int mid = len/2 - 1;
+        int max = Integer.MIN_VALUE;
+        ListNode node = head;
+        while(i <= mid){
+            st.push(node.val);
+            node = node.next;
+            i++;    
+        }
+        
+        while(node != null){
+            int num1 = st.pop();
+            int num2 = node.val;
+            max = Math.max(max, (num1 + num2));
+            node = node.next;
+        }
+        
+        return max;
+    }
+} 
