959a4ab2c9d8cdbd609354be4a2f09cf062444e3
Manu-Dev-Arya <manudevarya26@gmail.com>
2026-06-14 10:11:37 +0530

    Leetcode 2130

---
+++ LinkedList/2130. Maximum Twin Sum of a Linked List.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public int pairSum(ListNode head) {
+        int len = 0;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            len++;
+        }
+
+        int i = 0;
+        int mid = len/2 - 1;
+        int max = Integer.MIN_VALUE;
+        ListNode node = head;
+        while(i <= mid){
+            int num1 = node.val;
+            temp = node;
+            int k = len-1-i-i;
+            while(k > 0){
+                temp = temp.next;
+                k--;
+            }
+            int num2 = temp.val;
+            max = Math.max(max, (num1 + num2));
+            node = node.next;
+            i++;
+        }
+        return max;
+    }
+}
