51d67ebe78b982da6483e59870d1f97044f95f24
Manu-Dev-Arya <manudevarya26@gmail.com>
2026-07-16 10:08:10 +0530

    Leetcode 3867

---
+++ Arrays/3867. Sum of GCD of Formed Pairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+        int max = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            max = Math.max(num, max);
+            prefixGcd[i] = gcd(num, max);
+        }
+        Arrays.sort(prefixGcd);
+        int start = 0;
+        int end = n-1;
+        long sum = 0;
+        while(start < end){
+            int num1 = prefixGcd[start];
+            int num2 = prefixGcd[end];
+            sum += (long)gcd(num1, num2);
+            start++;
+            end--;
+        }
+        return sum;
+    }
+    public int gcd(int x, int y){
+        return y == 0 ? x : gcd(y, x % y);
+    }
+}
